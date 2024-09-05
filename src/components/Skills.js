e024e54837b4a3eeaeba9f9eb539362920f4a878
jiwonjeon <j.jeon339@mybvc.ca>
2024-09-05 14:11:39 -0600

    Change skills section design

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,5 +1,4 @@
 import { Container, Col, Row } from "react-bootstrap";
-import "react-multi-carousel/lib/styles.css";
 import java from "../assets/img/java.svg";
 import javascript from "../assets/img/javascript.svg";
 import typescript from "../assets/img/typescript.svg";
@@ -13,13 +12,10 @@ import react from "../assets/img/react.svg";
 import docker from "../assets/img/docker.svg";
 import git from "../assets/img/git.svg";
 import github from "../assets/img/github.svg";
-import jquery from "../assets/img/jquery.svg";
 import kotlin from "../assets/img/kotlin.svg";
 import mongodb from "../assets/img/mongodb.svg";
 import mysql from "../assets/img/mysql.svg";
 import amazon from "../assets/img/amazon.svg";
-
-import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const programmingLanguages = [
@@ -50,59 +46,39 @@ export const Skills = () => {
       <Container>
         <div className="skill-bx">
           <h2>Skills</h2>
-          <Row className="skill-tb">
-            <Col className="skill-pl" xs={12} md={6}>
+          <div className="skill-tb">
+            <div className="skill-pl">
               <p className="skill-header">Programming Languages</p>
-              <div className="skill-section">
-                <Row className="justify-content-center">
-                  {programmingLanguages.map((skill, index) => (
-                    <Col
-                      key={index}
-                      xs={6}
-                      sm={4}
-                      md={3}
-                      className="d-flex justify-content-center skill-icon"
-                    >
-                      <img
-                        src={skill.src}
-                        alt={skill.alt}
-                        className="icon-img"
-                      />
-                    </Col>
-                  ))}
-                </Row>
+              <div className="skill-section d-flex flex-wrap justify-content-center">
+                {programmingLanguages.map((skill, index) => (
+                  <div
+                    key={index}
+                    className="skill-icon d-flex flex-column align-items-center"
+                  >
+                    <img src={skill.src} alt={skill.alt} className="icon-img" />
+                    <span className="skill-name">{skill.alt}</span>
+                  </div>
+                ))}
               </div>
-            </Col>
-            <Col className="skill-dev" xs={12} md={6}>
+            </div>
+
+            <div className="skill-dev">
               <p className="skill-header">Web / App Developments</p>
-              <div className="skill-section">
-                <Row className="justify-content-center">
-                  {webAppDevelopments.map((skill, index) => (
-                    <Col
-                      key={index}
-                      xs={6}
-                      sm={4}
-                      md={3}
-                      className="d-flex justify-content-center skill-icon"
-                    >
-                      <img
-                        src={skill.src}
-                        alt={skill.alt}
-                        className="icon-img"
-                      />
-                    </Col>
-                  ))}
-                </Row>
+              <div className="skill-section d-flex flex-wrap justify-content-center">
+                {webAppDevelopments.map((skill, index) => (
+                  <div
+                    key={index}
+                    className="skill-icon d-flex flex-column align-items-center"
+                  >
+                    <img src={skill.src} alt={skill.alt} className="icon-img" />
+                    <span className="skill-name">{skill.alt}</span>
+                  </div>
+                ))}
               </div>
-            </Col>
-          </Row>
+            </div>
+          </div>
         </div>
       </Container>
-      <img
-        className="background-image-left"
-        src={colorSharp}
-        alt="Background decoration"
-      />
     </section>
   );
 };
