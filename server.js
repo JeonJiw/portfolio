57ee1f8e3a43b0e39e1f1e88e4fca6ceb4b4eba6
jiwonjeon <j.jeon339@mybvc.ca>
2024-09-05 15:49:32 -0600

    Add nodemailer

--- server.js
+++ server.js
@@ -1,52 +1,48 @@
 const express = require("express");
-const router = express.Router();
-const cors = require("cors");
 const nodemailer = require("nodemailer");
-
-// server used to send send emails
+const cors = require("cors");
 const app = express();
+const port = 8088;
+
 app.use(cors());
-app.use(express.json());
-app.use("/", router);
-app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
-
-const contactEmail = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "********@gmail.com",
-    pass: "",
-  },
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
 });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});
+function sendEmail(body) {
+  return new Promise(() => {
+    console.log(body);
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "jw.jeon24@gmail.com",
+        pass: "qgrz cnoz tjle kwkk",
+      },
+    });
 
-router.post("/contact", (req, res) => {
-  const name = req.body.firstName + req.body.lastName;
-  const email = req.body.email;
-  const message = req.body.message;
-  const phone = req.body.phone;
-  const mail = {
-    from: name,
-    to: "********@gmail.com",
-    subject: "Contact Form Submission - Portfolio",
-    html: `<p>Name: ${name}</p>
-           <p>Email: ${email}</p>
-           <p>Phone: ${phone}</p>
-           <p>Message: ${message}</p>`,
-  };
-  contactEmail.sendMail(mail, (error) => {
-    if (error) {
-      res.json(error);
-    } else {
-      res.json({ code: 200, status: "Message Sent" });
-    }
+    const mail_configs = {
+      to: "jw.jeon24@gmail.com",
+      from: body.email,
+      subject: `Email from ${body.name}`,
+      text: `This email fron ${body.email} // ${body.message}`,
+    };
+    transporter.sendMail(mail_configs, function (err, info) {
+      if (err) {
+        console.log(err);
+        return reject({ message: "Error" });
+      }
+      return resolve({ message: "Email sent successfully" });
+    });
   });
+}
+
+app.post("/", (req, res) => {
+  sendEmail(req.body.body.formDetails)
+    .then((response) => response.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
 });
+
+app.listen(port, () => console.log("Server Running"));
--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,22 +1,66 @@
 import { forwardRef } from "react";
+import { useState } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
+import axios from "axios";
+import { BodyText } from "react-bootstrap-icons";
 
 export const ContactMe = forwardRef((props, ref) => {
+  const formInitialDetails = {
+    name: "",
+    email: "",
+    message: "",
+  };
+
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
+
+  const onFormUpdate = (category, value) => {
+    setFormDetails({
+      ...formDetails,
+      [category]: value,
+    });
+  };
+
+  const handleSubmit = () => {
+    axios
+      .post("http://localhost:8088/", {
+        body: {
+          formDetails,
+        },
+      })
+      .then(() => {
+        console.log("success");
+      })
+      .catch(() => {
+        console.log("failure");
+      });
+  };
   return (
     <section id="contactme" ref={ref}>
       <Container className="contact">
-        <Row className="justify-content-center">
+        <Row className="justify-content-center contact-container">
           <Col md={8}>
             <h2>Contact Me</h2>
-            <Form className="form-container">
+            <Form className="form-container" onSubmit={handleSubmit}>
               <Form.Group controlId="formName">
                 <Form.Label>Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter your name" />
+                <Form.Control
+                  type="text"
+                  value={formDetails.name}
+                  placeholder="Enter your name"
+                  onChange={(e) => onFormUpdate("name", e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group controlId="formEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter your email" />
+                <Form.Control
+                  type="email"
+                  value={formDetails.email}
+                  placeholder="Enter your email"
+                  onChange={(e) => onFormUpdate("email", e.target.value)}
+                />
               </Form.Group>
 
               <Form.Group controlId="formMessage">
@@ -24,13 +68,24 @@ export const ContactMe = forwardRef((props, ref) => {
                 <Form.Control
                   as="textarea"
                   rows={10}
+                  value={formDetails.message}
                   placeholder="Enter your message"
+                  onChange={(e) => onFormUpdate("message", e.target.value)}
                 />
               </Form.Group>
 
               <button variant="primary" type="submit" className="mt-3">
-                Submit
+                <span>{buttonText}</span>
               </button>
+              {status.message && (
+                <Col>
+                  <p
+                    className={status.success === false ? "danger" : "success"}
+                  >
+                    {status.message}
+                  </p>
+                </Col>
+              )}
             </Form>
           </Col>
         </Row>
