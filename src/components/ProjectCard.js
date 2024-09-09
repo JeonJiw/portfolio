dc9f0bb9a94efe72ef745d2dc3ddf64d1a895818
jiwonjeon <j.jeon339@mybvc.ca>
2024-09-09 15:01:47 -0600

    Changed Projects Section layout

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,16 @@
-import { Col } from "react-bootstrap";
+import { Col, Card } from "react-bootstrap";
+import arrow from "../assets/img/arrow2.svg";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, devType, description, imgUrl }) => {
   return (
-    <Col sm={6} md={4}>
-      <div className="proj-imgbx">
-        <img src={imgUrl} />
-        <div className="proj-txtx">
-          <h4>{title}</h4>
-          <span>{description}</span>
-        </div>
-      </div>
+    <Col className="crousel-card">
+      <Card className="proj-card">
+        <Card.Img className="proj-imgbx" src={imgUrl} />
+        <Card.Body>
+          <Card.Title className="proj-txtx">{title}</Card.Title>
+          <Card.Text className="proj-devType">{devType}</Card.Text>
+        </Card.Body>
+      </Card>
     </Col>
   );
 };
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,83 +1,63 @@
-import { Container, Row, Col, Nav, Tab } from "react-bootstrap";
+import { Container, Row, Col, Carousel } from "react-bootstrap";
 import projImg1 from "../assets/img/project-img1.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import { ProjectCard } from "./ProjectCard";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 
 export const Projects = () => {
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Microsoft Team Auto Recording System",
+      devType: "Web Development",
+      description: "Web Development",
       imgUrl: projImg1,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Ther Genius Game",
+      devType: "Web Development",
+      description: "Web Development",
       imgUrl: projImg2,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "SafeCam",
+      devType: "Web Development",
+      description: "Web Development",
       imgUrl: projImg3,
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Calory Calculator",
+      devType: "Web Development",
+      description: "Android App Development",
       imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
     },
   ];
+  const projectChunks = [];
+  for (let i = 0; i < projects.length; i += 3) {
+    projectChunks.push(projects.slice(i, i + 3));
+  }
 
   return (
-    <section className="project" id="project">
+    <section className="project" id="projects">
       <Container>
         <Row>
           <Col>
             <h2>Projects</h2>
-            <p>description</p>
-            <Tab.Container id="projects-tabs" defaultActiveKey="first">
-              <Nav
-                variant="pills"
-                className="nav-pills mb-5 justify-content-center align-items-center"
-                id="pills-tab"
-              >
-                <Nav.Item>
-                  <Nav.Link eventKey="first">Tab 1</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="second">Tab 2</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                </Nav.Item>
-              </Nav>
-              <Tab.Content>
-                <Tab.Pane eventKey="first">
-                  <Row>
-                    {projects.map((project, index) => {
-                      return <ProjectCard key={index} {...project} />;
-                    })}
+            <Carousel indicators={false} controls={true} interval={null}>
+              {projectChunks.map((chunk, index) => (
+                <Carousel.Item key={index}>
+                  <Row className="justify-content-center">
+                    {chunk.map((project, idx) => (
+                      <Col key={idx} sm={6} md={4} className="card-wrapper">
+                        <ProjectCard {...project} />
+                      </Col>
+                    ))}
                   </Row>
-                </Tab.Pane>
-                <Tab.Pane eventKey="second">project</Tab.Pane>
-                <Tab.Pane eventKey="third">project</Tab.Pane>
-              </Tab.Content>
-            </Tab.Container>
+                </Carousel.Item>
+              ))}
+            </Carousel>
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2} />
     </section>
   );
 };
